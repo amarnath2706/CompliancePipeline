9427bb5aeffcb68e4074eab4d4310c12120a99fb
amarnath2706 <amarnath.arjsd@gmail.com>
2026-04-01 03:31:38 +0530

    commit

--- Complaince_Pipeline/backend/scripts/index_documents.py
+++ Complaince_Pipeline/backend/scripts/index_documents.py
@@ -22,3 +22,12 @@ def index_documents():
     #define paths and look for data folder
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_folder = os.path.join(current_dir,"../../backend/data")
+
+    #check the environment variables
+    logger.info("="*60)
+    logger.info("Checking environment variables...")
+    logger.info(f"AZURE_OPENAI_ENDPOINT: {os.getenv('AZURE_OPENAI_ENDPOINT')}")
+    logger.info(f"AZURE_OPENAI_API_VERSION: {os.getenv('AZURE_OPENAI_API_VERSION')}")
+    logger.info(f"Embedding Deployment: {os.getenv('AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME','text-embedding-3-small')}")
+    logger.info(f"AZURE_SEARCH_ENDPOINT: {os.getenv('AZURE_SEARCH_ENDPOINT')}")
+    logger.info(f"AZURE_SEARCH_INDEX_NAME: {os.getenv('AZURE_SEARCH_INDEX_NAME')}")
